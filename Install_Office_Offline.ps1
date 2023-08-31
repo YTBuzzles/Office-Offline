a94a17d3aabc6a172f045e9ba8cc080d59641738
YTBuzzles <bentory15@gmail.com>
2023-09-01 09:21:47 +1000

    trying to fix

--- Install_Office_Offline.ps1
+++ Install_Office_Offline.ps1
@@ -25,95 +25,100 @@ $window.ClientSize = '550, 500'
 
 $Instructions = New-Object System.Windows.Forms.TextBox
 $Instructions.Text = 
-"Select which programs you want to install, more common ones are highlighted in blue.
-They will all be installed automatically and activated using a script from https://massgrave.dev/"
+"
+ 1. Change installer files directory if needed
+ 2. If download button doesn't say done click it
+ 3. Choose desired applications
+ 4. Choose activation method
+ 5. Click install
+"
 $Instructions.Location = New-Object System.Drawing.Point(10, 5)
 $Instructions.Width = 530
-$Instructions.Height = 100
+$Instructions.Height = 110
 $Instructions.font = New-Object System.Drawing.Font("Segoe UI",12,[System.Drawing.FontStyle]::Regular)
 $Instructions.Multiline = $true
 
 $checkbox0 = New-Object System.Windows.Forms.CheckBox
 $checkbox0.Name = "Access2021Volume"
 $checkbox0.Text = "Access"
-$checkbox0.Location = New-Object System.Drawing.Point(20,110)
+$checkbox0.Location = New-Object System.Drawing.Point(20,120)
 
 $checkbox1 = New-Object System.Windows.Forms.CheckBox
 $checkbox1.Name = "Excel2021Volume"
 $checkbox1.Text = "Excel"
 $checkbox1.BackColor = "#cee5ed"
 $checkbox1.ForeColor = "#107aa1"
-$checkbox1.Location = New-Object System.Drawing.Point(20,135)
+$checkbox1.Location = New-Object System.Drawing.Point(20,145)
 
 $checkbox2 = New-Object System.Windows.Forms.CheckBox
 $checkbox2.Name = "OneNoteVolume"
 $checkbox2.Text = "OneNote"
-$checkbox2.Location = New-Object System.Drawing.Point(20,160)
+$checkbox2.Location = New-Object System.Drawing.Point(20,170)
 
 $checkbox3 = New-Object System.Windows.Forms.CheckBox
 $checkbox3.Name = "Outlook2021Volume"
 $checkbox3.Text = "Outlook"
-$checkbox3.Location = New-Object System.Drawing.Point(20,185)
+$checkbox3.Location = New-Object System.Drawing.Point(20,195)
 
 $checkbox4 = New-Object System.Windows.Forms.CheckBox
 $checkbox4.Name = "PowerPoint2021Volume"
 $checkbox4.Text = "PowerPoint"
 $checkbox4.BackColor = "#cee5ed"
 $checkbox4.ForeColor = "#107aa1"
-$checkbox4.Location = New-Object System.Drawing.Point(20,210)
+$checkbox4.Location = New-Object System.Drawing.Point(20,220)
 
 $checkbox5 = New-Object System.Windows.Forms.CheckBox
 $checkbox5.Name = "Publisher2019Volume"
 $checkbox5.Text = "Publisher"
 $checkbox5.BackColor = "#cee5ed"
 $checkbox5.ForeColor = "#107aa1"
-$checkbox5.Location = New-Object System.Drawing.Point(20,235)
+$checkbox5.Location = New-Object System.Drawing.Point(20,245)
 
 $checkbox6 = New-Object System.Windows.Forms.CheckBox
 $checkbox6.Name = "SkypeForBusiness2021Volume"
 $checkbox6.Text = "SkypeForBuisness"
 $checkbox6.Width = 300
-$checkbox6.Location = New-Object System.Drawing.Point(20,260)
+$checkbox6.Location = New-Object System.Drawing.Point(20,270)
 
 $checkbox7 = New-Object System.Windows.Forms.CheckBox
 $checkbox7.Name = "Word2021Volume"
 $checkbox7.Text = "Word"
 $checkbox7.BackColor = "#cee5ed"
 $checkbox7.ForeColor = "#107aa1"
-$checkbox7.Location = New-Object System.Drawing.Point(20,285)
+$checkbox7.Location = New-Object System.Drawing.Point(20,295)
 
 $checkbox8 = New-Object System.Windows.Forms.CheckBox
 $checkbox8.Name = "ProjectPro2021Volume"
 $checkbox8.Text = "Project Pro"
-$checkbox8.Location = New-Object System.Drawing.Point(20,310)
+$checkbox8.Location = New-Object System.Drawing.Point(20,320)
 
 $checkbox9 = New-Object System.Windows.Forms.CheckBox
 $checkbox9.Name = "ProjectStd2021Volume"
 $checkbox9.Text = "Project Standard"
 $checkbox9.Width = 300
-$checkbox9.Location = New-Object System.Drawing.Point(20,335)
+$checkbox9.Location = New-Object System.Drawing.Point(20,345)
 
 $checkbox10 = New-Object System.Windows.Forms.CheckBox
 $checkbox10.Name = "VisioPro2021Volume"
 $checkbox10.Text = "Visio Pro"
-$checkbox10.Location = New-Object System.Drawing.Point(20,360)
+$checkbox10.Location = New-Object System.Drawing.Point(20,370)
 
 $checkbox11 = New-Object System.Windows.Forms.CheckBox
 $checkbox11.Name = "VisioStd2021Volume"
 $checkbox11.Text = "Visio Standard"
 $checkbox11.Width = 300
-$checkbox11.Location = New-Object System.Drawing.Point(20,385)
+$checkbox11.Location = New-Object System.Drawing.Point(20,395)
 
 $checkbox12 = New-Object System.Windows.Forms.CheckBox
 $checkbox12.Name = "OneDrive" # ExcludedApps=OneDrive line in file
 $checkbox12.Text = "OneDrive Desktop"
 $checkbox12.Width = 300
-$checkbox12.Location = New-Object System.Drawing.Point(20,410)
+$checkbox12.Location = New-Object System.Drawing.Point(20,420)
 
 $checkbox13 = New-Object System.Windows.Forms.CheckBox
 $checkbox13.Text = "Microsoft Teams" # not sure for this one
 $checkbox13.Width = 300
-$checkbox13.Location = New-Object System.Drawing.Point(20,435)
+$checkbox13.Location = New-Object System.Drawing.Point(20,445)
 
 $checkbox14 = New-Object System.Windows.Forms.CheckBox
 $checkbox14.Text = "Activate With Renewal"
@@ -139,9 +144,9 @@ $installButton.ForeColor             = "#000"
 $DownloadFiles                       = New-Object system.Windows.Forms.Button
 $DownloadFiles.BackColor             = "#ffffff"
 $DownloadFiles.text                  = "Download"
-$DownloadFiles.width                 = 110
-$DownloadFiles.height                = 50
-$DownloadFiles.location              = New-Object System.Drawing.Point(400,250)
+$DownloadFiles.width                 = 130
+$DownloadFiles.height                = 50 
+$DownloadFiles.location              = New-Object System.Drawing.Point(390,250)
 $DownloadFiles.Font                  = 'Microsoft Sans Serif,10'
 $DownloadFiles.ForeColor             = "#000"
 
@@ -154,25 +159,61 @@ $exit.location              = New-Object System.Drawing.Point(440,440)
 $exit.Font                  = 'Microsoft Sans Serif,10'
 $exit.ForeColor             = "#000"
 
-function exiting {
-$window.Dispose()
-echo "All finished"
-}
-$exit.Add_Click({exiting})
+$changeDirectory                       = New-Object system.Windows.Forms.Button
+$changeDirectory.BackColor             = "#ffffff"
+$changeDirectory.text                  = "Installer Directory"
+$changeDirectory.width                 = 300
+$changeDirectory.height                = 50
+$changeDirectory.location              = New-Object System.Drawing.Point(200,130)
+$changeDirectory.Font                  = 'Microsoft Sans Serif,10'
+$changeDirectory.ForeColor             = "#000"
+
+$window.Controls.AddRange(@($checkbox0,$checkbox1,$checkbox2,$checkbox3,$checkbox4,$checkbox5,$checkbox6,$checkbox7,$checkbox8,$checkbox9,$checkbox10,
+    $checkbox11,$checkbox12,$checkbox13,$Instructions,$installButton,$checkbox14,$DownloadFiles,$changeDirectory,$exit,$checkbox15))
+
 
 $allowInstall = $false
+
 function updateDownload {
-if ($downloaded){
+# check if the installation files are already downloaded
+$downloaded = $true
+if ((gci -Filter "C2R_Monthly").Name -eq "C2R_Monthly" -and (Get-ChildItem ("$(pwd)" + "\$(gci -Filter "C2R_Monthly")") | measure Length -s).sum /1Kb -gt 0){
 $DownloadFiles.ForeColor = "#1d8223"
 $DownloadFiles.text = "Done"
 $allowInstall = $true
 }
+else {
+$DownloadFiles.ForeColor = "#000000"
+$DownloadFiles.text = "Download"
 }
-updateDownload
+}
 
-$downloaded = $true
+function chooseDirectory(){
+# now ask for directory to run installer in, not where office products will be installed
+$FileBrowser = New-Object System.Windows.Forms.FolderBrowserDialog
+$FileBrowser.Description = "Choose where to put installer files.
+This is not where programs will be installed"
+$FileBrowser.SelectedPath = (New-Object -ComObject Shell.Application).NameSpace('shell:Downloads').Self.Path
+$null = $FileBrowser.ShowDialog()
+
+# change to chosen directory
+cd $FileBrowser.SelectedPath
+$changeDirectory.Text = $FileBrowser.SelectedPath
+
+updateDownload
+}
+$changeDirectory.Add_Click({chooseDirectory})
+
+
 function download {
-if (!$downloaded){
+# first download the generator, configurator and installer files
+irm https://raw.githubusercontent.com/YTBuzzles/Office-Offline/main/YAOCTRI_Installer.cmd -OutFile "YAOCTRI_Installer.cmd"
+irm https://raw.githubusercontent.com/YTBuzzles/Office-Offline/main/YAOCTRU_Generator.cmd -OutFile "YAOCTRU_Generator.cmd"
+irm https://raw.githubusercontent.com/YTBuzzles/Office-Offline/main/YAOCTRI_Configurator.cmd -OutFile "YAOCTRI_Configurator.cmd"
+
+$DownloadFiles.ForeColor = "#1d8223"
+$DownloadFiles.text = "DOWNLOADING"
+
 # Run Generator file which creates script to download
 # offline install files with choices already selected
 .\YAOCTRU_Generator.cmd
@@ -188,35 +229,22 @@ echo ""
 Start-Process -FilePath ((gci -Filter "*en-US_Monthly*").Name) -Wait
 
 echo "Installation files downloaded"
-echo ""
-echo "Choosing Configuration"
-echo ""
 updateDownload
-}
 }
 $DownloadFiles.Add_Click({download})
 
-$changeDirectory                       = New-Object system.Windows.Forms.Button
-$changeDirectory.BackColor             = "#ffffff"
-$changeDirectory.text                  = "Directory"
-$changeDirectory.width                 = 300
-$changeDirectory.height                = 50
-$changeDirectory.location              = New-Object System.Drawing.Point(200,120)
-$changeDirectory.Font                  = 'Microsoft Sans Serif,10'
-$changeDirectory.ForeColor             = "#000"
-
-$window.Controls.AddRange(@($checkbox0,$checkbox1,$checkbox2,$checkbox3,$checkbox4,$checkbox5,$checkbox6,$checkbox7,$checkbox8,$checkbox9,$checkbox10,
-    $checkbox11,$checkbox12,$checkbox13,$Instructions,$installButton,$checkbox14,$DownloadFiles,$changeDirectory,$exit,$checkbox15))
 
 
+################################# Improve this I guess ##########################################
 function install{
-    if ($allowInstall) {
+    if (1 -eq 1) {
+    $installButton.text = "Installing"
+    $installButton.ForeColor = "#1d8223"
     # Use name to add to SKU list
     # now for logic
     $skus = "SKUs="
     for ($i=0; $i -lt 12; $i++)
     {
-    
         $varName = "checkbox" + $i
         $variable = (Get-Variable -Include $varName)
         if ($variable.Value.CheckState){
@@ -269,30 +297,13 @@ function install{
 $InstallButton.Add_Click({install})
 
 
-function changeDirectory(){
-# now ask for directory to run installer in, not where office products will be installed
-$FileBrowser = New-Object System.Windows.Forms.FolderBrowserDialog
-$FileBrowser.SelectedPath = (New-Object -ComObject Shell.Application).NameSpace('shell:Downloads').Self.Path
-$null = $FileBrowser.ShowDialog()
-
-# change to chosen directory
-cd $FileBrowser.SelectedPath
-$changeDirectory.Text = $FileBrowser.SelectedPath
-
-# check if the installation files are already downloaded
-$downloaded = $true
-if ((gci -Filter "C2R_Monthly").Name -ne "C2R_Monthly"){
-$downloaded = $false
+function exiting {
+$window.Dispose()
+echo "All finished"
 }
-# now download all other helper functions from github (already downloaded ones will be overwritten)
-irm https://raw.githubusercontent.com/YTBuzzles/Office-Offline/main/YAOCTRI_Installer.cmd -OutFile "YAOCTRI_Installer.cmd"
-irm https://raw.githubusercontent.com/YTBuzzles/Office-Offline/main/YAOCTRU_Generator.cmd -OutFile "YAOCTRU_Generator.cmd"
-irm https://raw.githubusercontent.com/YTBuzzles/Office-Offline/main/YAOCTRI_Configurator.cmd -OutFile "YAOCTRI_Configurator.cmd"
-updateDownload
-}
-$changeDirectory.Add_Click({changeDirectory})
+$exit.Add_Click({exiting})
 
-
+chooseDirectory
 $SKU = $window.ShowDialog()
 
 ####################################################################
